6a7bb3d0fb7d8314042d98a56cd2473d2c8af6cd
ShonChassel <gershoni1234@gmail.com>
2023-03-12 13:19:21 +0200

    fix bug

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -5,10 +5,11 @@ import Image from "next/image";
 import React, { useEffect } from 'react';
 import Loader from "../../components/Loader";
 
-export default function signin({ providers },) {
+ function Signin({ providers },) {
   const router = useRouter();
   const { data: session } = useSession()
   console.log('not in', session)
+  console.log('providers', providers)
   
   useEffect(() => {
     console.log('session:', session)
@@ -18,7 +19,7 @@ export default function signin({ providers },) {
   }, [session]);
   
 
-  // if (session) return <Loader />;
+  if (session) return <Loader />;
 
   return (
     <div className="bg-black h-screen flex flex-col items-center pt-40 space-y-8">
@@ -39,7 +40,7 @@ export default function signin({ providers },) {
         <div key={provider.id}>
           <button
             className="text-white py-4 px-6 rounded-full bg-[#1db954] transition duration-300 ease-out border border-transparent uppercase font-bold text-xs md:text-base tracking-wider hover:scale-105 hover:bg-[#0db146]"
-            onClick={() => signIn(provider.id)}
+            onClick={() => signIn("spotify")}
           >
             Sign in with {provider.name}
           </button>
@@ -49,7 +50,7 @@ export default function signin({ providers },) {
   );
 }
 
-// export default Signin;
+export default Signin;
 
 export async function getServerSideProps() {
   const providers = await getProviders();
